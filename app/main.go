8b54c3a6dff26f0b6c2375bb21eefc2c1acae669
9bany <bany.y0599@gmail.com>
2023-02-16 21:39:27 +0700

    fix: init

---
+++ app/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	ginprometheus "github.com/zsais/go-gin-prometheus"
+)
+
+func main() {
+	r := gin.Default()
+
+	p := ginprometheus.NewPrometheus("gin")
+
+	p.Use(r)
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hello	",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
